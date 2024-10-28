241b1466eca48fe5b035369494635792bfef8016
Tamiele <tammaro_85@hotmail.it>
2024-10-28 15:29:45 +0100

    inizio

---
+++ src/app/models/iaccess-data.ts
@@ -0,0 +1,6 @@
+import { iUser } from './i-user';
+
+export interface IaccessData {
+  accessToken: string;
+  user: iUser;
+}
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,9 +1,17 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { JwtHelperService } from '@auth0/angular-jwt';
+import { environment } from '../../environments/environment.development';
+import { Router } from '@angular/router';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
+  constructor(private http: HttpClient, private router: Router) {}
 
-  constructor() { }
+  jwtHelper: JwtHelperService = new JwtHelperService();
+
+  registerUrl: string = environment.registerUrl;
+  loginUrl: string = environment.loginUrl;
 }
