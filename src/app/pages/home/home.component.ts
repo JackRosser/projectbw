cf597aac46264670a14879fbec744e0b078c402e
Valerio Persiani <persianivalerio0@gmail.com>
2024-10-30 15:54:11 +0100

    avatar collegati ad id

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -4,16 +4,28 @@ import { AuthService } from '../../services/auth.service';
 import { UserService } from '../../services/user.service';
 import { iFavoriteUser } from '../../models/i-favorite-user';
 import { environment } from '../../../environments/environment.development';
-import { BehaviorSubject, tap } from 'rxjs';
+import { BehaviorSubject } from 'rxjs';
 import { FavoriteService } from '../../services/favorite.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss',
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
+<<<<<<< HEAD
+  arrUsers$ = new BehaviorSubject<iUser[]>([]); // Lista degli utenti osservabile
+  currentIndex: number = 0; // Indice dell'utente corrente
+  userId: number | null = null; // ID dell'utente loggato
+  isFavorite: boolean = false; // Stato di "preferito" dell'utente corrente
+  likeUsers: iUser[] = []; // Utenti aggiunti ai preferiti
+  previousMatchedUsers$ = new BehaviorSubject<number[]>([]); // Lista di utenti già scartati osservabile
+  noMoreUsers: boolean = false; // Flag per la fine della lista utenti
+  userFavUrl = environment.userFavUrl; // URL dei preferiti
+=======
   arrUsers: iUser[] = [];
+allUsersList!: iUser[]
+
 
   currentIndex: number = 0;
 
@@ -27,70 +39,86 @@ export class HomeComponent implements OnInit {
   private favoritesSubject = new BehaviorSubject<iFavoriteUser[]>([]);
 
   userFavUrl = environment.userFavUrl;
+>>>>>>> 1e8a2b2caf3344d3cb3371813cf859f5df6d4dbc
 
   constructor(
     private authSvc: AuthService,
     private userSvc: UserService,
     private favoriteSvc: FavoriteService
   ) {}
+
   ngOnInit(): void {
+<<<<<<< HEAD
+    // Ottieni dettagli dell'utente loggato
+=======
+   this.userSvc.allUser$.subscribe(list => {
+    this.allUsersList = list
+
+  })
+>>>>>>> 1e8a2b2caf3344d3cb3371813cf859f5df6d4dbc
     this.authSvc.user$.subscribe((user) => {
       if (user) {
         this.userId = user.id;
-        this.loadUsers();
+        this.loadUsers(); // Carica utenti se loggato
       } else {
-        // Se nessun utente è loggato, cancella il localStorage
+        // Rimuovi dal localStorage se nessun utente è loggato
         localStorage.removeItem('arrUsers');
         localStorage.removeItem('matchedUsers');
       }
     });
 
-    // Se l'utente già esiste
+    // Carica utenti e match precedenti dal localStorage
     const storedUsers = localStorage.getItem('arrUsers');
     const storedMatchedUsers = localStorage.getItem('matchedUsers');
     if (storedUsers) {
-      this.arrUsers = JSON.parse(storedUsers);
+      this.arrUsers$.next(JSON.parse(storedUsers));
     }
     if (storedMatchedUsers) {
-      this.previousMatchedUsers = JSON.parse(storedMatchedUsers);
+      this.previousMatchedUsers$.next(JSON.parse(storedMatchedUsers));
     }
   }
 
+  // Carica gli utenti e filtra in base a preferiti e match precedenti
   loadUsers() {
     this.favoriteSvc.getFavoritesForCurrentUser().subscribe((favorites) => {
       this.favoriteSvc.updateFavoritesInStorage(favorites);
-      const previousMatchedUsers = favorites.map((fav) => fav.user.id);
+      const previousMatchedIds = favorites.map((fav) => fav.user.id);
       this.userSvc.getUsers().subscribe((users: iUser[]) => {
-        this.arrUsers = users.filter((u) => u.id !== this.userId);
-        // Filtra gli utenti che sono già stati matchati
-        this.arrUsers = this.arrUsers.filter(
-          (user) => !previousMatchedUsers.includes(user.id)
+        const filteredUsers = users.filter(
+          (u) => u.id !== this.userId && !previousMatchedIds.includes(u.id)
         );
+<<<<<<< HEAD
+        this.updateArrUsers(filteredUsers);
+=======
+        console.log("INIZIO", this.arrUsers);
+
         // Salva l'elenco degli utenti aggiornato nel localStorage
         localStorage.setItem('arrUsers', JSON.stringify(this.arrUsers));
+>>>>>>> 1e8a2b2caf3344d3cb3371813cf859f5df6d4dbc
         this.updateFavoriteStatus();
       });
     });
   }
 
+  // Passa all'utente successivo
   nextUser() {
-    if (this.arrUsers.length > 0) {
-      this.currentIndex = (this.currentIndex + 1) % this.arrUsers.length;
+    const users = this.arrUsers$.value;
+    if (users.length > 0) {
+      this.currentIndex = (this.currentIndex + 1) % users.length;
       this.updateFavoriteStatus();
     } else {
       this.currentIndex = 0;
     }
   }
 
+  // Aggiunge un utente ai preferiti
   addToFavorites() {
-    if (
-      this.userId === null ||
-      this.arrUsers[this.currentIndex].nickname === null
-    ) {
+    const users = this.arrUsers$.value;
+    if (this.userId === null || !users[this.currentIndex]?.nickname) {
       console.error('Qualcosa è andato storto');
       return;
     }
-    const addedUser = this.arrUsers[this.currentIndex];
+    const addedUser = users[this.currentIndex];
     const favoriteUser: iFavoriteUser = {
       userId: this.userId,
       user: addedUser,
@@ -98,8 +126,7 @@ export class HomeComponent implements OnInit {
     this.favoriteSvc.getFavoritesForCurrentUser().subscribe(
       (favorites) => {
         const isAlreadyFavorite = favorites.some(
-          (fav) =>
-            fav.user.email === addedUser.email && fav.userId === this.userId
+          (fav) => fav.user.email === addedUser.email && fav.userId === this.userId
         );
         if (isAlreadyFavorite) {
           console.log("L'utente è già nei preferiti per questo userId");
@@ -110,48 +137,78 @@ export class HomeComponent implements OnInit {
           () => {
             this.isFavorite = true;
             console.log('Utente aggiunto ai preferiti');
-            // Aggiungi l'utente ai matchedUsers
             this.likeUsers.push(addedUser);
-            this.previousMatchedUsers.push(addedUser.id);
-            localStorage.setItem(
-              'matchedUsers',
-              JSON.stringify(this.previousMatchedUsers)
-            );
+            this.addMatchedUser(addedUser.id);
             this.removeMatchedUser();
           },
           (error) => {
             console.error("Errore durante l'aggiunta ai preferiti:", error);
-            this.removeMatchedUser(); // Assicurati di rimuovere l'utente anche se c'è un errore
+            this.removeMatchedUser();
           }
         );
       },
       (error) => {
         console.error('Errore nel recupero dei preferiti:', error);
-        this.removeMatchedUser(); // Assicurati di rimuovere l'utente anche se c'è un errore
+        this.removeMatchedUser();
       }
     );
   }
 
+  // Aggiorna lo stato di preferito
   private updateFavoriteStatus() {
-    if (this.arrUsers.length > 0) {
-      const user = this.arrUsers[this.currentIndex];
+    const users = this.arrUsers$.value;
+    if (users.length > 0) {
+      const user = users[this.currentIndex];
       this.favoriteSvc.isFavorite(user.id).subscribe((isFav) => {
         this.isFavorite = isFav;
       });
     }
   }
 
+  // Aggiunge l'utente corrente ai preferiti
   likeUser() {
     this.addToFavorites();
     console.log('Utenti piaciuti:', this.likeUsers);
+
+    if(this.arrUsers.length === 0) {
+      this.popolation();
+    }
+    console.log("ARRAY SPOPOLATO", this.arrUsers);
+    console.log("ARRAY CLONE", this.allUsersList);
+
   }
 
+<<<<<<< HEAD
+  // Scarta l'utente corrente
+=======
+
+>>>>>>> 1e8a2b2caf3344d3cb3371813cf859f5df6d4dbc
   dislikeUser() {
-    this.previousMatchedUsers.push(this.arrUsers[this.currentIndex].id);
+    const userId = this.arrUsers$.value[this.currentIndex].id;
+    this.addMatchedUser(userId);
     this.removeMatchedUser();
   }
 
+<<<<<<< HEAD
+  // Rimuove l'utente corrente e passa al successivo
   private removeMatchedUser() {
+    const updatedUsers = [...this.arrUsers$.value];
+    updatedUsers.splice(this.currentIndex, 1);
+    this.updateArrUsers(updatedUsers);
+
+    if (updatedUsers.length === 0) {
+      this.currentIndex = 0;
+      this.noMoreUsers = true;
+    } else if (this.currentIndex >= updatedUsers.length) {
+      this.currentIndex = 0;
+=======
+private popolation():void {
+this.arrUsers = this.allUsersList
+}
+
+
+  private removeMatchedUser() {
+
     const removedUser = this.arrUsers.splice(this.currentIndex, 1)[0];
     if (removedUser) {
       this.previousMatchedUsers.push(removedUser.id);
@@ -159,13 +216,22 @@ export class HomeComponent implements OnInit {
     if (this.arrUsers.length === 0) {
       this.currentIndex = 0;
     } else if (this.currentIndex >= this.arrUsers.length) {
-      this.currentIndex = 0;
+       this.currentIndex = 0;
+>>>>>>> 1e8a2b2caf3344d3cb3371813cf859f5df6d4dbc
     }
-    localStorage.setItem('arrUsers', JSON.stringify(this.arrUsers));
-    localStorage.setItem(
-      'matchedUsers',
-      JSON.stringify(this.previousMatchedUsers)
-    );
     this.nextUser();
+  }
+
+  // Aggiorna l'array degli utenti nel BehaviorSubject e nel localStorage
+  private updateArrUsers(newUsers: iUser[]) {
+    this.arrUsers$.next(newUsers);
+    localStorage.setItem('arrUsers', JSON.stringify(newUsers));
+  }
+
+  // Aggiunge un ID utente all'elenco dei match precedenti
+  private addMatchedUser(userId: number) {
+    const updatedMatchedUsers = [...this.previousMatchedUsers$.value, userId];
+    this.previousMatchedUsers$.next(updatedMatchedUsers);
+    localStorage.setItem('matchedUsers', JSON.stringify(updatedMatchedUsers));
   }
 }
--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -3,18 +3,32 @@ import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
 import { iUser } from '../models/i-user';
 import { environment } from '../../environments/environment.development';
-import { Observable } from 'rxjs';
+import { BehaviorSubject, Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class UserService {
-  constructor(private http: HttpClient, router: Router) {}
+  constructor(private http: HttpClient, router: Router) {this.getAllUsers()}
 
-  userUrl = environment.userUrl;
+  private usersBh = new BehaviorSubject<iUser[]>([])
+  allUser$ = this.usersBh.asObservable()
+
+
+  getAllUsers():void {
+    this.http.get<iUser[]>(environment.userUrl).subscribe(list => {
+      this.usersBh.next(list)
+    })
+    }
 
   getUsers(): Observable<iUser[]> {
-    return this.http.get<iUser[]>(this.userUrl);
+    return this.http.get<iUser[]>(environment.userUrl);
   }
 
+
+
 }
+
+
+
+
