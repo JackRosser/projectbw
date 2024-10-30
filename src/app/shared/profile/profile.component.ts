a18a6720208675c230d5026f0672e168120c23a5
Tamiele <tammaro_85@hotmail.it>
2024-10-30 17:43:29 +0100

    profilo

--- src/app/shared/profile/profile.component.ts
+++ src/app/shared/profile/profile.component.ts
@@ -1,13 +1,26 @@
-import { Component, Input, SimpleChanges } from '@angular/core';
+import { Component, Input, OnInit, SimpleChanges } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
+import { iUser } from '../../models/i-user';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
-  styleUrl: './profile.component.scss'
+  styleUrl: './profile.component.scss',
 })
-export class ProfileComponent {
+export class ProfileComponent implements OnInit {
   isFavoriteCollapsed = true;
   isChatCollapsed = true;
+  user!: iUser;
+  constructor(private authSvc: AuthService) {}
+
+  ngOnInit(): void {
+    this.authSvc.user$.subscribe((user) => {
+      if (user) {
+        this.user = user;
+      }
+      console.log(this.user);
+    });
+  }
 
   toggleFavorite() {
     this.isFavoriteCollapsed = !this.isFavoriteCollapsed;
@@ -21,20 +34,25 @@ export class ProfileComponent {
     if (!this.isChatCollapsed) {
       this.isFavoriteCollapsed = true; // Chiude il pannello Lista Preferiti
     }
-}
+  }
 
-@Input() profileOn!:boolean
-appear:string = "transform: translateX(100vw); transition: transform; transition-duration: 300ms;"
+  @Input() profileOn!: boolean;
+  appear: string =
+    'transform: translateX(100vw); transition: transform; transition-duration: 300ms;';
 
-ngOnChanges(changes: SimpleChanges) {
-  if (changes['profileOn'] && changes['profileOn'].currentValue !== undefined) {
-    if (this.profileOn === true) {
-      this.appear = "transform: translateX(0); transition: transform 300ms; ";
-      this.profileOn = false;
-    } else {
-      this.appear = "transform: translateX(100%); transition: transform 300ms;";
-      this.profileOn = true;
+  ngOnChanges(changes: SimpleChanges) {
+    if (
+      changes['profileOn'] &&
+      changes['profileOn'].currentValue !== undefined
+    ) {
+      if (this.profileOn === true) {
+        this.appear = 'transform: translateX(0); transition: transform 300ms; ';
+        this.profileOn = false;
+      } else {
+        this.appear =
+          'transform: translateX(100%); transition: transform 300ms;';
+        this.profileOn = true;
+      }
     }
   }
-}
 }
